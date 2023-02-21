ab258ed81cfeb1ce12a6b35accb0b9b52b8a5aad
KatiePee <piele.katie@gmail.com>
2023-02-21 15:16:47 -0800

    shift and unshift methods completed

--- dynamic-array.js
+++ dynamic-array.js
@@ -32,13 +32,33 @@ class DynamicArray {
   }
 
   shift() {
-
-    // Your code here
+   
+    if(this.length === 0) return undefined;
+  
+    this.length--;
+    let shifted = this.data[0];
+    
+    for(let i = 0; i < this.length; i ++){
+      this.data[i] = this.data[i+1]
+    }
+    return shifted
+    
   }
 
   unshift(val) {
+    //return the length
+    //iterate thru this.data to shift everything to the right
+    //add val to index 0
+    //add length
+    //return length
 
-    // Your code here
+    for(let i = this.length ; i > 0; i--){
+      this.data[i] = this.data[i-1]
+    }
+    this.data[0] = val;
+    this.length++;
+    return this.length;
+    
   }
 
   indexOf(val) {
